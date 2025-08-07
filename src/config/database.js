eac99c4fe296fb1c5fdb41d0ccaf9929d4db03d8
Ramiro <ramiroroman306@gmail.com>
2025-08-07 10:04:05 -0300

    Se establecio la conexion con la base de datos y se crearon los modelos user y task

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+// Importaciones 
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    },
+    
+);
+
+export const startDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Conexion exitosa con la base de datos");
+        await sequelize.sync();
+    } catch (error) {
+        console.log("Error en la conexion con la base de datos:", error)
+    };
+};
