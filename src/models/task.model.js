eb0bc7d6a4cce6b1599ebf9853e8bc0a2739040b
Ramiro <ramiroroman306@gmail.com>
2025-08-17 10:12:48 -0300

    relacion de Task

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
+import { User } from "./user.model.js";
 
 export const Task = sequelize.define(
     "Task",
@@ -9,3 +10,5 @@ export const Task = sequelize.define(
         isComplete: { type: DataTypes.BOOLEAN, defaultValue: false }
     }
 );
+
+Task.belongsTo(User, {foreignKey: "user_id", as: "user"})
